5e583fce60234b17de642e0d2d9d71732598b5d2
Hồ Đắc Minh Quân <hd.minhquan98@gmail.com>
2023-12-04 10:38:53 +0700

    refactor: separate file controllers and routes

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'server error',
+    message: 'The route not defined yet!',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'server error',
+    message: 'The route not defined yet!',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'server error',
+    message: 'The route not defined yet!',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'server error',
+    message: 'The route not defined yet!',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'server error',
+    message: 'The route not defined yet!',
+  });
+};
